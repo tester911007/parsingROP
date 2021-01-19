80b85e70b06c3d79d7fdaa0714205163d8feb838
tester911007 <vladmvm2020@gmail.com>
2021-01-19 21:13:34 +0300

    Из каждой папки взять последний по дате файл

--- src/main/java/parsing/Parsing.java
+++ src/main/java/parsing/Parsing.java
@@ -8,30 +8,69 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
 public class Parsing {
-    static String[] allParams = new String[57];
-    static ConnectionDB insertIntoDB = new ConnectionDB();
+    private static String initialPath = "C:\\Работа\\Export\\Записка расчётная\\Тестирование\\СД";
+    private static ArrayList<Path> allProvenPaths = new ArrayList<>();
+    private static String[] allParams = new String[57];
+    private static ConnectionDB insertIntoDB = new ConnectionDB();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+//        File initialPath = new File("S:\\a.senkevich\\");
+//        getFolders(initialPath);
         printPath();
     }
 
+    /*
+        public static void getFolders(File folder) throws IOException {
+            File[] folderEntries = folder.listFiles();
+            for (File entry : folderEntries) {
+                if (entry.isDirectory()) {
+                    findLastModifiedFile(entry.getAbsolutePath());
+                }
+            }
+        }
+
+        public static void findLastModifiedFile(String pathFolder) throws IOException {
+            Path dir = Paths.get(pathFolder);
+            if (Files.isDirectory(dir)) {
+                Optional<Path> opPath = Files.list(dir)
+                        .filter(p -> !Files.isDirectory(p))
+                        .filter(p -> p.getFileName().toString().startsWith("ROPC"))
+                        .sorted((p1, p2) -> Long.valueOf(p2.toFile().lastModified())
+                                .compareTo(p1.toFile().lastModified()))
+                        .findFirst();
+
+                if (opPath.isPresent()) {
+                    getDataFromFile(opPath.get());
+                }
+            }
+        }
+    */
     private static void printPath() {
-        try (Stream<Path> paths = Files.walk(Paths.get("C:\\Работа\\Export\\Записка расчётная\\Тестирование\\СД"))) {
+        try (Stream<Path> paths = Files.walk(Paths.get(initialPath))) {
             paths
                     .filter(p -> p.getFileName().toString().startsWith("ROPC"))
-                    .forEach(Parsing::getDataFromFile);
+                    .sorted((p1, p2) -> Long.compare(p2.toFile().lastModified(), p1.toFile().lastModified()))
+                    .forEach(Parsing::checkedPath);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
+    private static void checkedPath(Path path) {
+        if (!allProvenPaths.contains(path.getParent())) {
+            allProvenPaths.add(path.getParent());
+            getDataFromFile(path);
+        }
+    }
+
     private static void getDataFromFile(Path path) {
-        System.out.println("\n\n" + path);
+        System.out.println(path);
         try (BufferedReader reader = new BufferedReader(new FileReader(String.valueOf(path)))) {
             String line;
             int i = 1;
@@ -43,14 +82,12 @@ public class Parsing {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
         allParams[0] = "1";
         insertIntoDB.executeSQL(allParams);
     }
 
     private static void parseLine(String line, int position) {
         String regex = "(\\s*)=(\\s*)([0-9.]+)";
-
         Pattern pattern = Pattern.compile(regex, Pattern.MULTILINE);
         Matcher matcher = pattern.matcher(line);
 
